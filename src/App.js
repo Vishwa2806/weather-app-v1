fcb7fc8281f3534d82e7602058f8b660a046495d
tony000-JV <tony.jv000@gmail.com>
2024-02-07 19:56:19 +0530

    new file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SearchBar from './components/SearchBar';
+import FavoriteCities from './components/FavoriteCities';
+import './App.css'
 
-function App() {
+const App = () => {
+  const userId = '123';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 class="headding">Weather App</h1>
+      <SearchBar />
+      <FavoriteCities userId={userId} />
     </div>
   );
-}
+};
 
 export default App;
